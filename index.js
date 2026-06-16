44de4518cc8fd8b043464bb4cbc2c091ab3a6e78
Aditya Kumar <detectivecodes96@gmail.com>
2026-06-16 18:44:29 +0530

    Landing Page done

---
+++ index.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    // Finds the navigation menu
+    const menu = document.getElementById('nav-menu');
+    // Toggles the 'active' class on and off
+    menu.classList.toggle('active');
+}
